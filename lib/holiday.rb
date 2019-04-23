5f65f376e0482d34260468a6bcc20a00b8b5374c
Erin Stalzer <erinstalzer@gmail.com>
2019-04-23 18:43:19 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -60,7 +60,7 @@ def all_supplies_in_holidays(holiday_hash)
     puts "#{season.to_s.capitalize}:"
 binding.pry
     holidays.each do |day, supplies|
-      puts "   #{holiday.to_s.split("_").map { |h| h.capitalize }.join(" ")}: #{supplies.join(" ")}"
+      puts "   #{holiday.to_s.split("_").map { |h| h.capitalize }.join(" ")}: #{supplies.join(", ")}"
     end
   end
 
